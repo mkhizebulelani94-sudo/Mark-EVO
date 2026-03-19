055501ccc95fb96d0303eab85a726c16c363b568
mkhizebulelani94-sudo <mkhizebulelani94@gmail.com>
2026-03-19 08:42:13 +0200

    First Commit of my application

---
+++ app/src/main/java/com/example/markevaluation/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.markevaluation
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.markevaluation.ui.theme.MarkEvaluationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val marksInput = findViewById<EditText>(R.id.editMarks)
+        val resultText = findViewById<TextView>(R.id.txtResult)
+        val button = findViewById<Button>(R.id.btnCheck)
+        val resetButton = findViewById<Button>(R.id.txtstyle)
+
+
+        button.setOnClickListener {
+
+            val marks = marksInput.text.toString().toInt()
+
+            if (marks >= 80) {
+                resultText.text = "Symbol : A"
+
+            }
+            else if (marks >=70){
+                resultText.text = "Symbol : B"
+            }
+            else if (marks >=60){
+                resultText.text = "Symbol : C"
+            }
+            else if (marks >=50){
+                resultText.text = "Symbol : D"
+            }
+            else {
+                resultText.text = "Symbol : E"
+            }
+
+
+
+
+        }
+
+        //Reset Button
+        resetButton.setOnClickListener{
+            marksInput.text.clear()
+            resultText.text = "Result will appear here"
+        }
+    }
+}
+
